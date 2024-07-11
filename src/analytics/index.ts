eaff0ee581f01ab4e77efe2f8bfad41396de7979
Guillem Blasco <gowet92@gmail.com>
2024-07-11 20:57:40 +0200

    Add test countParagraphs function

--- src/analytics/index.ts
+++ src/analytics/index.ts
@@ -1,6 +1,28 @@
-export function countParagraphs(text: string): number {
-  const paragraphs = text
+export const countParagraphs = (text: string): number => {
+  const paragraphsCount = text
     .split(/\n\s*/)
     .filter((paragraph) => paragraph.trim().length > 0).length;
-  return paragraphs;
-}
+
+  return paragraphsCount;
+};
+
+export const countWords = (text: string): number => {
+  const wordsCount = text
+    .split(/\s|[^\wáéíóúüÁÉÍÓÚÜñÑàèìòùÀÈÌÒÙäëïöüÄËÏÖÜ\/\-]/)
+    .filter((word) => word.length > 0).length;
+
+  return wordsCount;
+};
+
+export const countCharacters = (text: string): number => {
+  const charactersCount = text.length;
+
+  return charactersCount;
+};
+export const getShortWords = (text: string, length: number = 4): number => {
+  const textWords = text
+    .split(/\s|[^\wáéíóúüÁÉÍÓÚÜñÑàèìòùÀÈÌÒÙäëïöüÄËÏÖÜ\/\-]/)
+    .filter((word) => word.length <= length).length;
+
+  return textWords;
+};
