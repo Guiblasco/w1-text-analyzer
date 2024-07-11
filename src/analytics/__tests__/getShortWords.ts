52485cbb85b5181bbdf5772b93f137f34a154c72
Guillem Blasco <gowet92@gmail.com>
2024-07-11 20:59:56 +0200

    Add test getShortWords function

---
+++ src/analytics/__tests__/getShortWords.ts
@@ -0,0 +1,41 @@
+import { getShortWords } from "../../analytics/index.js";
+
+const test1 = (): void => {
+  console.log("getShortWords function case 0");
+
+  const shortWord = "";
+  const shortWordsCount = getShortWords(shortWord);
+  const expectedShortWord = 0;
+
+  if (shortWordsCount === expectedShortWord) {
+    console.log("Test Passed");
+
+    return;
+  } else {
+    console.log("Test Failed");
+    console.log(getShortWords(shortWord));
+
+    return;
+  }
+};
+
+const test2 = (): void => {
+  console.log("getShortWords function case 2");
+
+  const shortWord = "hola como estas giliiii";
+  const shortWordsCount = getShortWords(shortWord);
+  const expectedShortWord = 2;
+
+  if (shortWordsCount === expectedShortWord) {
+    console.log("Test Passed");
+
+    return;
+  } else {
+    console.log("Test Failed");
+
+    return;
+  }
+};
+
+test1();
+test2();
